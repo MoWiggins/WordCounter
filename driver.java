5596e962466daa3d1e62a87efc5b764366e0e9e3
MoWiggins <69360951+MoWiggins@users.noreply.github.com>
2021-03-02 13:02:22 -0600

    Add files via upload
    adding the main file

---
+++ driver.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+import java.io.File;
+import java.util.HashMap;
+/**
+ *
+ * @author Mohamed Omar
+ */
+public class driver {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+       WordCounterGui myGui = new WordCounterGui();
+       myGui.setVisible(true);
+    }
+    
+}
